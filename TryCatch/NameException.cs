839a2b757869603c2dac94d65eb3a254773ce42a
anjali buddhe <anjalibuddhe124@gmail.com>
2023-07-13 09:21:33 +0530

    Add project files.

---
+++ TryCatch/NameException.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch
+{
+    public class NameException : Exception
+    {
+        public NameException(string message) : base(message)
+        {
+
+        }
+
+
+    }
+
+    public class Student
+    {
+        public string name;
+        public int age;
+
+        public void AcceptDeatils(string name, int age)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                throw new NameException("Name  not be null");
+            }
+            else
+            {
+                this.name = name;
+                this.age = age;
+            }
+        }
+
+    }
+   
+}
---
+++ TryCatch/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch
+
+
+
+{
+    public class Program
+    {
+        
+            static void Main(string[] args)
+            {
+                Student student = new Student();
+                try
+                {
+                    student.AcceptDeatils("", 20);
+                }
+                catch (NameException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+
